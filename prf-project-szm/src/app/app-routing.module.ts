d3c6b4901ac48411426747c4a2930aea23c6ac27
Szűcs Máté <h880786@stud.u-szeged.hu>
2022-04-02 15:50:32 +0200

    Generated most important components

--- prf-project-szm/src/app/app-routing.module.ts
+++ prf-project-szm/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { NotfoundComponent } from './notfound/notfound/notfound.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { LoginComponent } from './login/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomepageComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- prf-project-szm/src/app/app.module.ts
+++ prf-project-szm/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { NotfoundComponent } from './notfound/notfound/notfound.component';
+import { HomeComponent } from './home/home/home.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent,
+    HomeComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
